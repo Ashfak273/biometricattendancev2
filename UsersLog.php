87bd06fe3150c94bf601726d315026a4437b9f68
Ashfak273 <mzashfak@gmail.com>
2024-08-17 00:48:04 +0530

    small change

--- UsersLog.php
+++ UsersLog.php
@@ -56,7 +56,7 @@ if (!isset($_SESSION['Admin-name'])) {
 </head>
 <body>
 <?php include'header.php'; ?> 
-<section class="container py-lg-5">>
+<section class="container py-lg-5">
   <!--User table-->
     <h1 class="slideInDown animated">Students daily logs</h1>
     <div class="form-style-5">
